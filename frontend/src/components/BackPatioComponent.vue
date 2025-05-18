93b38aa71383a26a4e04cd717693e41ff5a05f15
João Victor Fretes <58962044+parazinshow@users.noreply.github.com>
2025-05-18 03:28:34 -0600

    Adding backpatio and inside components

--- frontend/src/components/BackPatioComponent.vue
+++ frontend/src/components/BackPatioComponent.vue
@@ -5,7 +5,143 @@
       maxHeight: '80vh',
     }"
   >
-    <v-row> </v-row>
+    <!-- T62, 63, 64, 65 -->
+    <v-row :style="{minHeight: '25vh'}">
+      <!-- T62 -->
+      <v-col sm="3"
+        ><v-card
+          class="round-table bg-tableEmpty"
+          :style="{
+            height: '15vh',
+            width: '8vw',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- T63 -->
+      <v-col sm="3"
+        ><v-card
+          class="bg-tableEmpty"
+          :style="{
+            height: '100%',
+            width: '40%',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- T64 -->
+      <v-col sm="3"
+        ><v-card
+          class="bg-tableEmpty"
+          :style="{
+            height: '65%',
+            width: '85%',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- T65 -->
+      <v-col sm="3" :style="{minHeight: '20vh'}"
+        ><v-card
+          class="bg-tableEmpty"
+          :style="{
+            height: '100%',
+            width: '35%',
+            margin: '0 auto',
+            transform: 'rotate(-45deg)',
+          }"
+        ></v-card
+      ></v-col>
+    </v-row>
+
+    <!-- T61, 69, 70 -->
+    <v-row :style="{minHeight: '25vh'}">
+      <!-- T61 -->
+      <v-col sm="2"
+        ><v-card
+          class="round-table bg-tableEmpty"
+          :style="{
+            height: '15vh',
+            width: '8vw',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <v-col sm="1"></v-col>
+      <!-- T69 -->
+      <v-col sm="3"
+        ><v-card
+          class="round-table bg-tableEmpty"
+          :style="{
+            height: '15vh',
+            width: '8vw',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- T70 -->
+      <v-col sm="2"
+        ><v-card
+          class="round-table bg-tableEmpty"
+          :style="{
+            height: '15vh',
+            width: '8vw',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <v-col sm="4"></v-col>
+    </v-row>
+
+    <!-- T60, 68, 67, 66 -->
+    <v-row :style="{minHeight: '25vh'}">
+      <!-- T60 -->
+      <v-col sm="4"
+        ><v-card
+          class="bg-tableEmpty"
+          :style="{
+            height: '70%',
+            width: '75%',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- Space for the door -->
+      <v-col sm="1"></v-col>
+      <!-- T68 -->
+      <v-col sm="2"
+        ><v-card
+          class="bg-tableEmpty"
+          :style="{
+            height: '100%',
+            width: '60%',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- T67 -->
+      <v-col sm="3"
+        ><v-card
+          class="bg-tableEmpty"
+          :style="{
+            height: '60%',
+            width: '70%',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+      <!-- T66 -->
+      <v-col sm="2"
+        ><v-card
+          class="round-table bg-tableEmpty"
+          :style="{
+            height: '60%',
+            width: '70%',
+            margin: '0 auto',
+          }"
+        ></v-card
+      ></v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -24,5 +160,11 @@
     align-items: center;
     justify-content: center;
     border-radius: 8px;
+  }
+  .round-table {
+    border-radius: 100%;
+  }
+  .v-row {
+    padding: 0px;
   }
 </style>
--- frontend/src/components/InsideComponent.vue
+++ frontend/src/components/InsideComponent.vue
@@ -5,7 +5,196 @@
       maxHeight: '80vh',
     }"
   >
-    <v-row> </v-row>
+    <v-row>
+      <!-- Left side -->
+      <v-col sm="6">
+        <!-- T1 and T2 -->
+        <v-row>
+          <!-- T1 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '100%',
+                width: '60%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+          <!-- T2 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '80%',
+                width: '30%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+        <!-- Space between T1 and T14 -->
+        <v-row>
+          <v-col :style="{minHeight: '25vh'}"></v-col>
+        </v-row>
+        <!-- T14 and T13 -->
+        <v-row>
+          <!-- T13 -->
+          <v-col sm="4" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '100%',
+                width: '55%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+          <!-- T14 -->
+          <v-col sm="8" :style="{minHeight: '20vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '80%',
+                width: '85%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+        <!-- T12 and T11 and T150 -->
+        <v-row>
+          <!-- T150 -->
+          <v-col sm="4" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '80%',
+                width: '50%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+          <!-- T12 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '100%',
+                width: '60%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+          <!-- T11 -->
+          <v-col sm="2" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '80%',
+                width: '100%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+      </v-col>
+
+      <!-- Right side -->
+      <v-col sm="6">
+        <!-- T4 and T5 -->
+        <v-row>
+          <!-- T5 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '90%',
+                width: '60%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+          <!-- T4 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '90%',
+                width: '60%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+        <!-- T6 -->
+        <v-row>
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '90%',
+                width: '60%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+        <!-- T7 -->
+        <v-row>
+          <v-col sm="6" :style="{minHeight: '15vh'}"></v-col>
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '70%',
+                width: '30%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+        <!-- T8 -->
+        <v-row>
+          <v-col sm="6" :style="{minHeight: '15vh'}"></v-col>
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '90%',
+                width: '70%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+        <!-- T9 and T10 -->
+        <v-row>
+          <!-- T10 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '90%',
+                width: '60%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+          <!-- T9 -->
+          <v-col sm="6" :style="{minHeight: '15vh'}"
+            ><v-card
+              class="bg-tableEmpty"
+              :style="{
+                height: '70%',
+                width: '30%',
+                margin: '0 auto',
+              }"
+            ></v-card
+          ></v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -24,5 +213,8 @@
     align-items: center;
     justify-content: center;
     border-radius: 8px;
+  }
+  .v-row {
+    padding: 0px;
   }
 </style>
